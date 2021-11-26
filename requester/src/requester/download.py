da3518578167c61cd31fbcfa1d0b9d329c3303f0
Diorge Brognara <diorge.bs@gmail.com>
2021-11-25 21:20:07 -0300

    Adding function to download an entire patch
    Still missing the tests for forward-robustness.
    I changed my idea of alternating the download;
    it now goes backwards until it downloads the entire patch,
    then starts going forward.

--- requester/src/requester/download.py
+++ requester/src/requester/download.py
@@ -1,11 +1,19 @@
+import itertools
 import typing
+import time
 
 import ratelimit
 import requests
+from loguru import logger
 
 GameID = typing.NewType("GameID", int)
 
 CALLS_PER_SECOND: int = 1
+DEFAULT_RETRY_ATTEMPTS = (0, 1, 2, 5, 10, 30)
+
+
+class TooManyTriesError(Exception):
+    pass
 
 
 @ratelimit.sleep_and_retry
@@ -15,6 +23,8 @@ def get_game_data(
     api_token: typing.Optional[str] = None,
     url: str = "https://open-api.bser.io/v1/games",
 ) -> requests.Response:
+    """Download all the data of a single match."""
+
     if api_token is None:
         with open("key.secret", "r") as f:
             api_token = f.read()
@@ -22,6 +32,58 @@ def get_game_data(
     headers = {"x-api-key": api_token, "accept": "application/json"}
     complete_url = f"{url}/{game_id}"
 
+    logger.debug(f"Requesting game_id=<{game_id}>")
     response = requests.get(complete_url, headers=headers)
 
     return response
+
+
+def download_patch(
+    starting_game_id: GameID,
+    retry_time_in_seconds: typing.Tuple[float, ...] = DEFAULT_RETRY_ATTEMPTS,
+) -> typing.Iterable[requests.Response]:
+    """
+    Downloads game matches from the patch of the given Game ID.
+    Will keep downloading at time intervals upon reaching 404 (or any error),
+    since it should be downloading the most current matches.
+    When moving forward it will also keep patch in mind,
+    not stepping boundaries into the next patch.
+    """
+
+    def get_patch(game_data) -> typing.Tuple[str, str]:
+        first_player = game_data["userGames"][0]
+        patch_version = first_player["versionMajor"]
+        hotfix_version = first_player["versionMinor"]
+        return (patch_version, hotfix_version)
+
+    starting_game = get_game_data(GameID(starting_game_id))
+    yield starting_game
+
+    target_patch = get_patch(starting_game.json())
+
+    def download_from(game_id_seq):
+        current_patch = target_patch
+
+        while current_patch == target_patch:
+            next_id = next(game_id_seq)
+
+            attempt = 0
+            successful = False
+            while not successful and attempt < len(retry_time_in_seconds):
+                time.sleep(retry_time_in_seconds[attempt])
+                next_game = get_game_data(GameID(next_id))
+                successful = (
+                    next_game.status_code == 200 and next_game.json()["code"] == 200
+                )
+                attempt += 1
+            if not successful:
+                raise TooManyTriesError(
+                    f"Maximum attempts at retrieving game data for game_id=<{next_id}>"
+                )
+
+            current_patch = get_patch(next_game.json())
+            if current_patch == target_patch:
+                yield next_game
+
+    yield from download_from(itertools.count(start=starting_game_id - 1, step=-1))
+    yield from download_from(itertools.count(start=starting_game_id + 1))
--- requester/src/requester/tests/test_api.py
+++ requester/src/requester/tests/test_api.py
@@ -4,7 +4,6 @@ import requests_mock
 
 import requester.download as dwn
 
-
 SAMPLE_GAME_ID = dwn.GameID(13594270)
 
 
@@ -39,3 +38,47 @@ def test_api_key_invalid() -> None:
     # just a check we're actually hitting the official API with our token
     response = dwn.get_game_data(SAMPLE_GAME_ID, api_token="SomeInvalidToken")
     assert response.status_code == 403
+
+
+def test_download_entire_patch() -> None:
+    """Is able to download all games of a patch, not touching other patches."""
+    fine_json = {"code": 200, "userGames": [{"versionMajor": 45, "versionMinor": 0}]}
+    old_json = {"code": 200, "userGames": [{"versionMajor": 44, "versionMinor": 0}]}
+    fail_json = {"code": 404}
+    with requests_mock.Mocker() as m:
+        m.get("https://open-api.bser.io/v1/games/10", json=fine_json)
+        m.get("https://open-api.bser.io/v1/games/11", json=fine_json)
+        m.get("https://open-api.bser.io/v1/games/12", json=fine_json)
+        m.get("https://open-api.bser.io/v1/games/9", json=old_json)
+        m.get("https://open-api.bser.io/v1/games/13", json=fail_json)
+
+        gen = []
+        try:
+            for game in dwn.download_patch(dwn.GameID(11), retry_time_in_seconds=(0,)):
+                gen.append(game)
+        except dwn.TooManyTriesError:
+            pass
+
+        assert 3 == len(gen)
+
+
+def test_download_stops_next_patch() -> None:
+    """Downloading the patch stops if the next game is next patch."""
+    fine_json = {"code": 200, "userGames": [{"versionMajor": 45, "versionMinor": 0}]}
+    old_json = {"code": 200, "userGames": [{"versionMajor": 44, "versionMinor": 0}]}
+    future_json = {"code": 200, "userGames": [{"versionMajor": 45, "versionMinor": 1}]}
+    with requests_mock.Mocker() as m:
+        m.get("https://open-api.bser.io/v1/games/10", json=fine_json)
+        m.get("https://open-api.bser.io/v1/games/11", json=fine_json)
+        m.get("https://open-api.bser.io/v1/games/12", json=fine_json)
+        m.get("https://open-api.bser.io/v1/games/9", json=old_json)
+        m.get("https://open-api.bser.io/v1/games/13", json=future_json)
+
+        gen = []
+        try:
+            for game in dwn.download_patch(dwn.GameID(11), retry_time_in_seconds=(0,)):
+                gen.append(game)
+        except dwn.TooManyTriesError:
+            pass
+
+        assert 3 == len(gen)
